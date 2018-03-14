d17d6b879ae3226e2890649362e3752dd9b56c38
Vincent Lalanne <v.lalanne@fugu.fr>
2018-03-14 13:11:53 +0100

    Fix the init command errors on files already exits

--- commands/init.js
+++ commands/init.js
@@ -13,29 +13,27 @@ initCommand.prototype.run = function(env) {
   // Get the settings
   var settings = utils.getSettings(env.configPath);
 
+  utils.bot('Generating wpleasefile and gitignore files...');
+
   // Generate wpleasefile in project
-  utils.bot('Generating wpleasefile...');
   fs.copyFile(
     path.resolve(`${path.dirname(__dirname)}/lib/wpleasefile.js`), 
     path.resolve(`${env.cwd}/wpleasefile.js`), 
     COPYFILE_EXCL,
     function(err) {
-      if (err) throw err;
-      console.log('Success: wpleasefile was generated in project.');
-      console.log('');
+      if (err) console.log('Info: wpleasefile already exits in project.');
+      else console.log('Success: wpleasefile was generated in project.');
     }
   );
 
   // Generate gitignore file in project
-  utils.bot('Generating .gitignore...');
   fs.copyFile(
     path.resolve(`${path.dirname(__dirname)}/lib/gitignore.txt`), 
     path.resolve(`${env.cwd}/.gitignore`), 
     COPYFILE_EXCL,
     function(err) {
-      if (err) throw err;
-      console.log('Success: .gitignore file was generated in project.');
-      console.log('');
+      if (err) console.log('Info: .gitnignore already exits in project.');
+      else console.log('Success: .gitignore was generated in project.');
     }
   );
   
