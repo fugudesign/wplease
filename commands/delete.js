43a7f5908976d40d1c294a65003ce6e08eefc31c
Fugu Design <fugudesign@mac.com>
2018-03-19 11:39:41 +0100

    Clean code

--- commands/delete.js
+++ commands/delete.js
@@ -1,20 +1,21 @@
 #!/usr/bin/env node
 
-'use strict';
-var utils = require('../lib/utils');
-var Enquirer = require('enquirer');
-var exec = require('child_process').exec;
+'use strict'
+var utils = require('../lib/utils')
+var Enquirer = require('enquirer')
+var exec = require('child_process').exec
 
 // create a new prompt instance
-var enquirer = new Enquirer();
-enquirer.register('confirm', require('prompt-confirm'));
+var enquirer = new Enquirer()
+enquirer.register('confirm', require('prompt-confirm'))
 
 // Command
-function DeleteCommand() {}
-DeleteCommand.prototype.run = function(env) {
+function DeleteCommand () {}
 
+DeleteCommand.prototype.run = function (env) {
+  
   /**
-   * Ask for confirmation about deleting all 
+   * Ask for confirmation about deleting all
    * gitignored files and folders
    */
   enquirer.ask({
@@ -23,25 +24,25 @@ DeleteCommand.prototype.run = function(env) {
     message: 'Are you sure you want to delete all git ignored files ? This action is irreversible.',
     default: false
   })
-  .then(function(answers) {
-    if (answers.delete) {
-      console.log('');
-      
-      utils.bot('Deleting git ignored files...');
-
-      // Delete all git ignored files and folders
-      exec('git clean -xdf', 
-      (err, stdout, stderr) => {
-          if (err instanceof Error) {
-              console.error(err);
-              throw err;
-          }
-          console.log('Success: Deleted git ignored files.');
-          console.log('');
-      });
-    }
-  });
+    .then(function (answers) {
+      if (answers.delete) {
+        console.log('')
+        
+        utils.bot('Deleting git ignored files...')
+        
+        // Delete all git ignored files and folders
+        exec('git clean -xdf',
+          (err, stdout, stderr) => {
+            if (err instanceof Error) {
+              console.error(err)
+              throw err
+            }
+            console.log('Success: Deleted git ignored files.')
+            console.log('')
+          })
+      }
+    })
 }
 
-var inst = new DeleteCommand();
-module.exports = inst;
+var inst = new DeleteCommand()
+module.exports = inst
